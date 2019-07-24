43437fe70d21980da1f373477a7d8e1098f2fdf3
Raj Rathod <raaajrathod@gmail.com>
2019-07-24 14:20:54 +0530

    Backend Server Setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server Started on PORT : ${PORT}`);
+});
